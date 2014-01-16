3bface4dea5a2ccbf48b0ecbb198772207afcfed
Denis Gatin <chizel@inbox.ru>
2014-01-16 08:41:39 +0200

    problem 40 solved

--- problem40.py
+++ problem40.py
@@ -8,25 +8,29 @@ def number_of_digits(number):
         i += 1
     return i
 
+def give_digit(number, position):
+    #0 = last digit, len(number) - 1 = first digit
+    for i in range(0, position):
+        number /= 10
+    return number % 10
+
 def main():
     i = 1
-    point = 10
     result = 1
+    digits_count = 0
+    point = 10
 
-    while(digits <= 1000000):
+    while(digits_count <= 1000000):
         nod_i = number_of_digits(i)
-        digits += nod_i
+        digits_count += nod_i
 
-        if digits >= point:
-            dig_pos = digits - point
-
-            if dig_pos:
-                pass
-            else:
-                result *= 
+        if digits_count >= point:
+            dig_pos = digits_count - point
+            digit = give_digit(i, dig_pos)
+            result *= digit
             point *= 10
-
         i += 1
+    print result
 
 if __name__ == "__main__":
     main()
