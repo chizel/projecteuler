77675d7608294457cb75caeb7762d9477076d5bc
Denis Gatin <chizel@inbox.ru>
2014-01-21 18:06:57 +0200

    add is_pandigital()

--- libpe.py
+++ libpe.py
@@ -11,7 +11,26 @@ def number_of_digits(number):
         number /= 10
     return res
 
+def is_pandigital(number, bound=9, include_zero=0, all_digits=1):
+    '''return True if number is pandigital
+    if number may have 0, set include_zero to 1
+    if number may have not all digits, set all_digits=0
+    '''
+    digits = list(str(number))
+
+    if not include_zero and '0' in digits:
+        return False
+
+    if all_digits and len(digits) < (9 + include_zero):
+        return False
+
+    if len(digits) > len(set(digits)):
+        return False
+    else:
+        return True
+
 def main():
+    print is_pandigital(123456789)
     return
 
 if __name__ == "__main__":
