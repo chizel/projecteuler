608bf5fd514ef3459b595991df08df9470e2fcd8
Denis Gatin <chizel@inbox.ru>
2014-01-21 17:49:50 +0200

    add function number_of_digits

--- libpe.py
+++ libpe.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def number_of_digits(number):
+    #number is 0 and has 1 digit
+    if not number:
+        return 1
+    res = 0
+    while number:
+        res += 1
+        number /= 10
+    return res
+
 def main():
     return
 
