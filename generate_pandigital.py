b22013137fdeb4c8d84960ef0b1d51be24514901
Denis Gatin <chizel@inbox.ru>
2014-01-23 19:50:35 +0200

    generate pandigital, not working yet :)

---
+++ generate_pandigital.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+
+def gen_pand(lbound=1, ubound=9):
+    s = range(lbound, ubound + 1)
+
+def main():
+    gen_pand()
+
+if __name__ == "__main__":
+    start  = time.time()
+    main()
+    print "Time: ", time.time() - start, " seconds."
+
