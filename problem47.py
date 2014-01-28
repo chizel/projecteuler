fe94da4f7f7774cb7e672841c10d91e445ceaa5c
Denis Gatin <chizel@inbox.ru>
2014-01-28 17:37:21 +0200

    problem 47

---
+++ problem47.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+def main():
+    return
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print 'Time: ', time.time() - start
+
