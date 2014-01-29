7c7e152878d86aa453ad791b227d0f3432716f66
Denis Gatin <chizel@inbox.ru>
2014-01-29 09:14:42 +0200

    still not working

--- problem47.py
+++ problem47.py
@@ -2,8 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import time
+import libpe
+
+def check_mult(number, primes):
+    num_div = 0
+
+    for prime in primes:
+        if prime > number:
+            return 0
+
+        #number divisible by prime
+        if not number % prime:
+            number /= prime
+            num_div += 1
+            
+            if num_div > 3:
+                return num_div
+    return 0
 
 def main():
+    primes = libpe.generate_primes(999)
+    cons = 0
+
+    for i in range(1000, 9999):
+        if check_mult(i, primes) > 3:
+            cons += 1
+        else:
+            cons = 0
+        if cons >= 2:
+            print i, i - 1, i - 2, 'answer: ', i - 3
+
     return
 
 if __name__ == "__main__":
