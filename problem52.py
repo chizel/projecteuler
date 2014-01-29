8c2b25f9da8470b4492cdc787cc40d3099b031b2
Denis Gatin <chizel@inbox.ru>
2014-01-29 17:59:54 +0200

    problem 52 solved

--- problem52.py
+++ problem52.py
@@ -3,8 +3,37 @@
 
 import time
 
+def digits(number):
+    count = 0
+    digits = []
+
+    while number:
+        digits.append(number % 10)
+        number /= 10
+    return digits
+
+def is_same_digits(numbers):
+    digits_list = []
+    digits_list.append(digits(numbers[0]))
+    digits_list[0].sort()
+    digits_count = len(digits_list[0]) 
+
+    for i in range(1, len(numbers)):
+        digits_list.append(digits(numbers[i]))
+        if not digits_count == len(digits_list[-1]):
+            return False
+        digits_list[-1].sort()
+
+    for i in range(1, len(digits_list)):
+        if not digits_list[0] == digits_list[i]:
+            return False
+    return True
+
 def main():
-    return
+    for i in range(100000, 999999):
+        if is_same_digits([i * j for j in range(1, 6)]):
+            print i
+            return
 
 if __name__ == "__main__":
     start = time.time()
