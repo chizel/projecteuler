1debdea0ac7d9864f839bb6d9f886987b9d638a7
Denis Gatin <chizel@inbox.ru>
2014-02-02 20:22:22 +0200

    not working yet

--- problem82.py
+++ problem82.py
@@ -4,23 +4,40 @@
 import time
 
 def find_path(numbers):
-    path = 0
 
 #len(tmp) == number of rows
-    tmp = []
-    for row in numbers:
-        print row
+    count_of_rows = len(numbers) 
+    count_of_columns = len(numbers[0])
 
+    for col in range(count_of_columns - 2, -1, -1):
+        tmp = []
+
+        for row in numbers:
+            tmp.append(row[col] + row[col + 1])
+
+        numbers[0][col] = tmp[0]
+        numbers[count_of_rows - 1][col] = tmp[count_of_rows - 1]
+
+        for i in range(1, count_of_rows - 1):
+            numbers[i][col] += min(tmp[i - 1], tmp[i + 1], numbers[i][col + 1])
+
+    path = numbers[0][0]
+
+    for row in numbers:
+        if path > row[0]:
+            path = row[0]
+            print row[0]
     return path
 
 def main():
-    numbers = [[131 ,673 ,234 ,103 ,18],
-               [201 ,96, 342 ,965 ,150],
-               [630 ,803 ,746 ,422 ,111],
-               [537 ,699 ,497 ,121 ,956],
-               [805 ,732 ,524 ,37,331]]
+    numbers = []
+
+    with open('matrix.txt', 'r') as f:
+        for line in f:
+            tmp_list = line.split(',')
+            numbers.append([int(x) for x in tmp_list])
+
     print find_path(numbers)
-    return
 
 if __name__ == "__main__":
     start = time.time()
