6fe70a60f562536b19a1b91ea388c835e7c15b28
Denis Gatin <chizel@inbox.ru>
2014-02-06 11:11:58 +0200

    works only with simple sudoku

--- problem96.py
+++ problem96.py
@@ -1,9 +1,147 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Sudoku():
+    '''problem 96'''
+
+    def __init__(self, lines):
+        self.__generate_sudoku(lines)
+        self.__generate_guess_nums()
+
+    def __generate_sudoku(self, lines):
+        self.sudoku = []
+
+        for i in range(0, len(lines)):
+            self.sudoku.append([])
+
+            for digit in lines[i]:
+                self.sudoku[i].append(int(digit))
+
+    def __generate_guess_nums(self):
+        self.guess_nums = []
+
+        for i in range(0,9):
+            self.guess_nums.append([])
+
+            for j in range(0, 9):
+                if self.sudoku[i][j]:
+                    self.guess_nums[i].append([])
+                else:
+                    self.guess_nums[i].append([x for x in range(1,10)])
+
+    def __what_square(self, row, column):
+        square = []
+
+        if row <= 2:
+            square.append([0,1,2])
+        elif row <= 5:
+            square.append([3,4,5])
+        else:
+            square.append([6,7,8])
+
+        if column <= 2:
+            square.append([0,1,2])
+        elif column <= 5:
+            square.append([3,4,5])
+        else:
+            square.append([6,7,8])
+
+        return square
+
+    def __delete_number(self, number, row, column):
+        square = self.__what_square(row, column)
+
+        for i in square[0]:
+
+            for j in square[1]:
+
+                if not self.guess_nums[i][j]:
+                    continue
+                if not number in self.guess_nums[i][j]:
+                    continue
+                self.guess_nums[i][j].remove(number)
+
+        for i in range(0, 9):
+            if not self.guess_nums[row][i]:
+                continue
+            if not number in self.guess_nums[row][i]:
+                continue
+            self.guess_nums[row][i].remove(number)
+
+        for i in range(0, 9):
+            if not self.guess_nums[i][column]:
+                continue
+            if not number in self.guess_nums[i][column]:
+                continue
+            self.guess_nums[i][column].remove(number)
+
+    def solve_sudoku(self):
+        for i in range(0,9):
+            for j in range(0,9):
+                if not self.sudoku[i][j]:
+                    continue
+                self.__delete_number(self.sudoku[i][j], i, j)
+
+        i = 0
+
+        while self.check():
+            i += 1
+            print i
+            pass
+    
+    def check_row(self, row):
+        for nums in self.guess_nums[row]:
+            print nums
+        pass
+
+    def check_column(self, column):
+        for row in self.guess_nums:
+            print row[column]
+
+    def check(self):
+        change = False
+
+        for i in range(0,9):
+            for j in range(0,9):
+                if len(self.guess_nums[i][j]) == 1:
+                    change = True
+                    self.sudoku[i][j] = self.guess_nums[i][j][0]
+                    self.__delete_number(self.sudoku[i][j], i, j)
+        return change
+        
+
+    def print_sudoku(self):
+        res = ''
+        res = '.___________.\n'
+
+        for i in range(0,9):
+
+            if i == 3 or i == 6:
+                res += '.---+---+---.\n'
+            res += '|'
+
+            for j in range(0,9):
+                res += str(self.sudoku[i][j])
+                if j == 2 or j == 5:
+                    res += '|'
+            res += '|\n'
+        res += '.-----------.\n\n'
+        print res
+
+    def print_guess_nums(self):
+        for line in self.guess_nums:
+            print line
+
 def main():
-    return
+    #source = '003020600 900305001 001806400 008102900 700000008 006708200 002609500 800203009 005010300'
+    source = '300200000 000107000 706030500 070009080 900020004 010800050 009040301 000702000 000008006'
+    lines = source.split()
+    
+    p = Sudoku(lines)
+    p.solve_sudoku()
+    #p.print_guess_nums()
+    #p.print_sudoku()
+    p.check_column(2)
 
 if __name__ == "__main__":
     main()
-
