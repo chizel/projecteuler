0715b9afdfc65c11bd1762d0ca6b7a2d0585b077
Denis Gatin <chizel@inbox.ru>
2014-03-07 08:26:48 +0200

    still slow

--- problem66.py
+++ problem66.py
@@ -12,32 +12,34 @@ class problem66():
         self.squares = [x**2 for x in xrange(1, border + 1)]
 
     def find_equation(self, d):
-        for x in xrange(0, len(self.squares)):
-            for y in xrange(x - 1, -1, -1):
-                result = self.squares[x] - d * self.squares[y]
-                if result > 1:
-                    break
-                elif result < 1:
-                    continue
-                else:
-                    return [sqrt(self.squares[x]), d]
-        return [0,0]
+        if d in self.squares:
+            return [d, 0]
+
+        for y in xrange(0, len(self.squares)):
+            mult = d * self.squares[y]
+
+            if mult > self.squares[-1]:
+                return [d, 0]
+            elif (mult + 1) in self.squares:
+                return [d, mult + 1]
+        return [d, 0]
 
     def find_solution(self):
         tmp = [0,0]
 
-        for d in xrange(2, 10):#01):
-            print d
+        for d in xrange(2, 1001):
             result = self.find_equation(d)
 
-            if result[0] > tmp[0]:
+            if result[1] > tmp[1]:
                 tmp = result[:]
+        tmp[1] = sqrt(tmp[1])
         return tmp
 
 def main():
     p = problem66()
-    p.generate_squares(20000)
-    print p.find_solution()
+    p.generate_squares(50000)
+    #print p.find_solution()
+    print p.find_equation(58)
     return
 
 if __name__ == "__main__":
