d2a488041a8d85fc9b0a9d060670d380962aabe2
Denis Gatin <chizel@inbox.ru>
2014-03-08 20:00:43 +0200

    in progress

--- problem69.py
+++ problem69.py
@@ -1,9 +1,26 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+import libpe
+
+BOUND = 10
+
 def main():
-    return
+    primes = libpe.generate_primes(BOUND)#, include_1=1) 
+    divisors = []
+
+    for i in xrange(2,BOUND):
+        tmp = []
+        for prime in primes:
+            if prime > (i / 2):
+                break
+            if not i % prime:
+                tmp.append(prime)
+        divisors.append(tmp)
+    print divisors
 
 if __name__ == "__main__":
+    start = time.time()
     main()
-
+    print 'Start: ', time.time() - start
