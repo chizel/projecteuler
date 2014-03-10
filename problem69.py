99bdf4da9db1e5488c408dde7d34b0909c763e09
Denis Gatin <chizel@inbox.ru>
2014-03-10 09:17:06 +0200

    before global changes

--- problem69.py
+++ problem69.py
@@ -6,24 +6,31 @@ from math import sqrt
 import libpe
 
 BOUND = 1000000
-#BOUND = 10
+BOUND = 10
 
 def main():
     primes = libpe.generate_primes(1000)#, include_1=1) 
-    divisors = []
+    divisors = [[],[]]
 
     for i in xrange(2,BOUND):
         tmp = []
+        
+        if i in primes:
+            divisors.append([i])
+            continue
+
+        primes_bound = int(sqrt(i))
 
         for prime in primes:
-            if prime > int(sqrt(i)):
+            if prime > primes_bound:
                 break
             while not i % prime:
                 i /= prime
                 tmp.append(prime)
         divisors.append(tmp)
-    #for i in range(len(divisors)):
-        #print i + 2, divisors[i]
+
+    for num in divisors:
+        print num
 
 if __name__ == "__main__":
     start = time.time()
