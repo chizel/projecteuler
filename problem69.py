edc800a122f483699891f3dd6da75692ef51282c
Denis Gatin <chizel@inbox.ru>
2014-03-12 21:53:55 +0200

    not solved

--- problem69.py
+++ problem69.py
@@ -7,7 +7,7 @@ import libpe
 
 BOUND = 1000000
 
-def is_have_some_numbers(a, b):
+def is_have_same_numbers(a, b):
     for number in a:
         if number in b:
             return True
@@ -29,25 +29,25 @@ def main():
                     i /= prime
         divisors.append(tmp)
 
-    maxz = [0,0]
-    for i in range(len(divisors)):
-        if maxz[0] < len(divisors[i]):
-            maxz[0] = len(divisors[i])
-            maxz[1] = i
-    print maxz
-    #count_rprimes = [1] * len(divisors)
-    #maxm = [0.0, 0]
+    #maxz = [0,0]
+    #for i in range(len(divisors)):
+        #if maxz[0] < len(divisors[i]):
+            #maxz[0] = len(divisors[i])
+            #maxz[1] = i
+    #print maxz
+    count_rprimes = [1] * len(divisors)
+    maxm = [0.0, 0]
     
 
-    #for i in range(BOUND - 2000, BOUND-1000,2):
-        #print i
-        #for j in range(3, i, 2):
-            #if not is_have_some_numbers(divisors[j], divisors[i]):
-                #count_rprimes[i] += 1
-        #if float(i) / count_rprimes[i] > maxm[0]:
-            #maxm[0] = float(i) / count_rprimes[i]
-            #maxm[1] = i
-    #print maxm
+    for i in range(51, BOUND, 51):#BOUND - 3000, BOUND-2000,51):
+        print i
+        for j in range(3, i, 2):
+            if not is_have_same_numbers(divisors[j], divisors[i]):
+                count_rprimes[i] += 1
+        if float(i) / count_rprimes[i] > maxm[0]:
+            maxm[0] = float(i) / count_rprimes[i]
+            maxm[1] = i
+    print maxm
 
 if __name__ == "__main__":
     start = time.time()
